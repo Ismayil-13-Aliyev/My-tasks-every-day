bc208d4d2ab1f2c07053d49fccb9f315c5c110b2
Ismayil-13-Aliyev <aze.ismayil13@gmail.com>
2026-02-21 19:46:54 +0400

    first time

---
+++ Lessons/Lesson_1/index.js
@@ -0,0 +1,58 @@
+//1
+
+let num = 239;
+
+if (num % 2 == 0) {
+    console.log("Chetnoe");
+} else {
+    console.log("Ne chetnoe");
+}
+
+//2
+
+let age = 27;
+
+if (age >= 18) {
+    console.log("Dostup razreshen");
+} else {
+    console.log("Dostup zapreshen");
+}
+
+//3
+
+let number = -990887;
+
+if (number > 0) {
+    console.log("Polojitelnor");
+} else if (number == 0) {
+    console.log("0");
+} else {
+    console.log("Otricatelnoe");
+}
+
+//4
+
+let a = 10;
+let b = 20;
+
+if (a > b) {
+    console.log("a > b");
+} else if (a < b) {
+    console.log("a < b");
+} else if (a == b) {
+    console.log("a = b");
+}
+
+//5
+
+let grade = 99;
+
+if (grade >= 90) {
+    console.log("Good");
+} else if (grade >= 70) {
+    console.log("Norm");
+} else if (grade >= 50) {
+    console.log("Not bad");
+} else {
+    console.log("Bad");
+}
