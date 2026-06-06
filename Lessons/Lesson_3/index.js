75dd0037d28ca0851693f469e5388307b75ba15d
Ismayil-13-Aliyev <aze.ismayil13@gmail.com>
2026-06-06 21:32:37 +0400

    Didn't finish but did 7 tascks

---
+++ Lessons/Lesson_3/index.js
@@ -0,0 +1,44 @@
+//1
+
+for(let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+//2
+
+for(let a = 10; a >= 1; a--) {
+    console.log(a);
+}
+
+//3
+
+let sum = 0;
+for(let b = 1; b <= 100; b++) {
+    sum += b;
+}
+console.log(sum);
+
+//4
+
+for(let c = 1; c <= 20; c++) {
+    if(c % 2 == 0) {
+        console.log(c);
+    }
+}
+
+//5
+
+for (let d = 1; d <= 7; d++) {
+    for(let e = 1; e <= 10; e++) {
+        console.log(d + " * " + e + " = " + d*e);
+    }
+}
+
+//6
+
+let num = 123456;
+console.log(num.toString().length);
+
+//7
+
+let n = 5;
